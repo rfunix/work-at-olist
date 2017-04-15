d17f27d6e99ded26d2d95841f35e1a61fa307560
Rafael Francischini <rfunix@Rafaels-MacBook-Air.local>
2017-04-15 15:42:48 -0300

    Create management import category from csv command

---
+++ work-at-olist/workatolist/management/commands/importcategories.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+from workatolist.models import Channel, Category
+
+class Command(BaseCommand):
+	help = """
+			  The Command received channel and csv to import categories
+			  """
+	def add_arguments(self, parser):
+		parser.add_argument("channel", nargs="+", type=str)
+		parser.add_argument("csv_file", nargs="+", type=str)
+
+	def handle(self, *args, **options):
+		channel = options["channel"][0]
+		csv_file = options["csv_file"][0]
