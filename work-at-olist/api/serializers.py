52f14e2e640d03f69da01b81b13c4a52050e989d
Rafael Francischini <rafinha.unix@gmail.com>
2017-04-16 12:31:56 -0300

    Create Serializer and use recursiveField

--- work-at-olist/api/serializers.py
+++ work-at-olist/api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 from workatolist import models
+from rest_framework_recursive.fields import RecursiveField
 
 
-class ChannelSerializer(serializers.HyperlinkedModelSerializer):
+class ChannelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Channel
         fields = ('id', 'name')
+
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    parent = RecursiveField(allow_null=True)
+
+    class Meta:
+        model = models.Category
+        fields = ('name', 'parent')
