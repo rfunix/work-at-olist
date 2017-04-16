3481a35a3f738d6a61e375d047aadc7fe44b0d87
Rafael Francischini <rafinha.unix@gmail.com>
2017-04-16 18:24:51 -0300

    Add single category search and channel categories search views

--- work-at-olist/api/views.py
+++ work-at-olist/api/views.py
@@ -1,8 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from workatolist import models
 from api import serializers
+import operator
+from django.db.models import Q
+import functools
 
 
 class ChannelListViewSet(viewsets.ModelViewSet):
     queryset = models.Channel.objects.all()
     serializer_class = serializers.ChannelSerializer
+
+
+class ChannelListCategories(generics.ListAPIView):
+    serializer_class = serializers.CategoriesSerializer
+
+    def get_queryset(self):
+        channel = models.Channel.objects.filter(name=self.kwargs['name'])
+        return models.Category.objects.filter(channel=channel,
+                                              level=0)
+
+
+class CategoriesView(generics.ListAPIView):
+    serializer_class = serializers.CategoriesSerializer
+
+    def get_queryset(self):
+        return models.Category.objects.filter(
+            name=self.kwargs['name']).get_ancestors(include_self=True)
