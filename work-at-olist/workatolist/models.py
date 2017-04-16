4ce18e656895f5e1b8eab5c7a9a99a7f866ce3fc
Rafael Francischini <rafinha.unix@gmail.com>
2017-04-16 12:32:38 -0300

    Change Category model to use django-mptt

--- work-at-olist/workatolist/models.py
+++ work-at-olist/workatolist/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Channel(models.Model):
@@ -8,10 +9,17 @@ class Channel(models.Model):
         db_table = "Channel"
 
 
-class Category(models.Model):
-    parent = models.ForeignKey("self", null=True)
+class Category(MPTTModel):
+    parent = TreeForeignKey('self', null=True, blank=True,
+                            db_index=True, default=None)
     name = models.CharField(max_length=30)
-    channel = models.ForeignKey(Channel)
+    channel = models.ForeignKey(Channel, related_name='categories')
 
     class Meta:
         db_table = "Category"
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __str__(self):
+        return '%d: %s' % (self.id, self.name)
